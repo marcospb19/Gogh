44f140a54312c949d3541e47105b8cc9d956a347
João Marcos <marcospb19@hotmail.com>
2020-05-28 19:51:31 -0300

    Added argparse function

--- termite-themes.py
+++ termite-themes.py
@@ -1,6 +1,15 @@
+import argparse
+
+
+def run_argparse() -> argparse.Namespace:
+    """ wrap argparse configuration """
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+
 def main():
     """ main function """
-    pass
+    args: argparse.Namespace = run_argparse()
 
 
 if __name__ == '__main__':
